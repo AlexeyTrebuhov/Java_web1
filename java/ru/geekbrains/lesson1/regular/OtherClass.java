af221173924cdf7323ff0c6d0e82d91e344d4fbc
Alex_T <saler@inbox.ru>
2023-05-29 00:06:27 +0300

    HW Java1

---
+++ java/ru/geekbrains/lesson1/regular/OtherClass.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.lesson1.regular;
+
+/**This comments
+ *
+ */
+
+public class OtherClass {
+
+    /**
+     * Summ
+     * @param a first parameter
+     * @param b second parameter
+     * @return sum a + b, no control.
+     */
+
+    public static int add(int a,int b){
+        return  a + b;
+    }
+
+    public static int div(int a,int b){
+        return  a / b;
+    }
+
+    public static int mul(int a,int b){
+        return  a * b;
+    }
+
+    public static int sub(int a,int b){
+        return  a - b;
+    }
+
+
+}
---
+++ java/ru/geekbrains/lesson1/sample/Main.java
@@ -0,0 +1,45 @@
+package ru.geekbrains.lesson1.sample;
+
+import ru.geekbrains.lesson1.regular.Decorator;
+import ru.geekbrains.lesson1.regular.OtherClass;
+
+/**
+ * javac -sourcepath ./java -d out java/ru/geekbrains/lesson1/sample/Main.java
+ *
+ * java -classpath ./out ru.geekbrains.lessonq.sample.Main
+ */
+
+
+/**
+ * FROM bellsoft/liberica-openjdk-alpine:latest
+ * COPY ./java ./src
+ * RUN mkdir ./out
+ * RUN javac -sourcepath ./src -d out src/ru/geekbrains/lesson1/sample/Main.java
+ * CMD java -classpath ./out ru.geekbrains.lesson1.sample.Main
+ */
+
+/**
+ * FROM bellsoft/liberica-openjdk-alpine:latest as BuildProject
+ * WORKDIR /app
+ * COPY ./java ./src
+ * RUN mkdir ./out
+ * RUN javac -sourcepath ./src -d out ./src/ru/geekbrains/lesson1/sample/Main.java
+ *
+ * FROM scratch as OutputFiles
+ * COPY --from=BuildProject /app/out /bin
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        int result = OtherClass.add (2,3);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.sub(2,3);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.mul(2,3);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.div(2,3);
+        System.out.println(Decorator.decorate(result));
+
+    }
+
+}
